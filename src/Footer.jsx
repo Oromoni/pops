487cbdd78f3891eae7f2b197804bfca42a494ad2
Oromoni <hazeloromoni@yahoo.com>
2025-02-06 00:02:07 +0100

    updated v

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
 import { FaPhone } from 'react-icons/fa'; // Import the phone icon from React Icons
+import { IoLocationOutline } from "react-icons/io5";
+import { CiMail } from "react-icons/ci";
+import { SiFireship } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-6 border-t border-black/20">
-      <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
-        <div>
-          <p className="text-gray-600 text-sm">© 2025 JOSHBIM</p>
-        </div>
-        <div className="flex items-center space-x-3" id='contact'>
-          {/* Telephone Icon and Phone Number */}
-          <FaPhone className="text-white" size={20} />
-          <p className="text-sm text-white">+234 803 823 0112</p>
+    <footer className="bg-black/95 text-white py-8 border-t border-black/20" id="contact">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
+        
+        {/* <div className="text-center md:text-left">
+          <p className="text-gray-400 text-sm">© 2025 JOSHBIM</p>
+        </div> */}
+
+      <div className="flex flex-col justify-center items-start "> <div className="flex items-center space-x-3">
+            <CiMail  className="text-white" size={15} />
+            <p className="text-white font-semibold">dummy@mail.com</p>
+</div>
+ <div className="flex items-center space-x-3">
+            <FaPhone className="text-white" size={15} />
+            <p className="text-white font-semibold">+234 803 823 0112</p>
+          </div>
+          </div>       
+
+          <div className="text-white/20">
+    <SiFireship size={30} />
+</div>
+
+
+
+        <div className="flex flex-col  items-start gap-2 text-sm">
+        
+          <div className="text-white text-center md:text-left flex gap-3 ">
+            <IoLocationOutline  size={20}  />
+            <p className="text-gray-400">3rd block, 9A Aerodrome Rd Apapa, <br />  Lagos, Nigeria</p> 
+          </div>
+         
         </div>
       </div>
     </footer>
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,20 +4,20 @@ import React from "react";
 const Hero = () => {
   return (
     <>
-      <div className=" p-2 bg-black" id="About">
-        <div className="text-white bg-black p-5 sm:flex sm:flex-row sm:gap-6 mt-4">
+      <div className=" p-2 bg-black/95" id="About">
+        <div className="text-white  p-5 sm:flex sm:flex-row sm:gap-6 mt-4">
           <h6 className="text-2xl font-bold sm:w-1/2" id="about">
             As a<span className="text-[#F6DB31]"> Marine </span>
             firm with 10+ years of industry excellence, we provide
             holistic and bespoke solutions for your watercraft shipping needs.
           </h6>
 
-          <p className="mt-2 sm:w-1/2 sm:text-sm">
-            <p className="">
-              We specialise in the providing logisitic for all kinds of Offshore vessels such as AHTS, PSV, Security vessels. Providing engineering service for offshore machinery eg expert in catapillar general overhauling of catapillar , wastila and Rolls Royce engines.
+          <div className="mt-2 sm:w-1/2 sm:text-sm text-5xl">
+            <p className="text-base">
+              We specialised in providing logisitic for all kinds of Offshore vessels such as AHTS, PSV, Security vessels. Providing engineering service for offshore machinery eg expert in catapillar general overhauling of catapillar , wastila and Rolls Royce engines.
             </p>
             <br />
-            <p className="mt-2">
+            <p className="mt-2 text-base">
               Working closely with owners, brokers, and manufacturers, our
               in-house team of professionals oversees the entire operation from
               planning to loading and delivery. We take charge of port
@@ -26,9 +26,9 @@ const Hero = () => {
               services.
             </p>
             <br />
-            <p className="mt-2 flex flex-col gap-4" id="service">
+            <p className="mt-2 flex flex-col gap-4 text-base" id="service">
              <li>
-              <span className="font-bold">
+              <span className="font-bold text-base">
                       Offshore Electrical & Logistic Support:&nbsp; </span>
 
   Specializing in offshore electrical installations, logistics for oil and gas, and providing green light/clearing for offshore services.</li>
@@ -48,7 +48,7 @@ Offshore Materials & Equipment Supply: &nbsp;
             </p>
             <br />
            
-          </p>
+          </div>
         </div>
       </div>
     </>
